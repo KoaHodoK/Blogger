5aaa384aa3b05461d1838c44519cdb4a8fcfbe88
KoaHodoK <koahodok@gmail.com>
2022-01-05 10:48:52 +0630

    image pick

--- lib/screens/add_post.dart
+++ lib/screens/add_post.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:blog_app/widgets/round_button.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class AddPost extends StatefulWidget {
   const AddPost({Key? key}) : super(key: key);
@@ -12,10 +13,62 @@ class AddPost extends StatefulWidget {
 
 class _AddPostState extends State<AddPost> {
   File? _image;
+  final _picker = ImagePicker();
   final _formkey = GlobalKey<FormState>();
   TextEditingController titleController = TextEditingController();
   TextEditingController descriptionController = TextEditingController();
   String title = "", description = "";
+
+  void dialog(context) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0)),
+              content: SizedBox(
+                  height: 120,
+                  child: Column(children: [
+                    ListTile(
+                        onTap: () {
+                          getCameraImage();
+                          Navigator.pop(context);
+                        },
+                        leading: const Icon(Icons.camera),
+                        title: const Text('Camera')),
+                    ListTile(
+                        onTap: () {
+                          getImageGallery();
+                          Navigator.pop(context);
+                        },
+                        leading: const Icon(Icons.photo_library),
+                        title: const Text('Gallery')),
+                  ])));
+        });
+  }
+
+  Future getImageGallery() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('Image no  selected!');
+      }
+    });
+  }
+
+  Future getCameraImage() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.camera);
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('Image no  selected!');
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,74 +86,80 @@ class _AddPostState extends State<AddPost> {
           ),
           centerTitle: true,
         ),
-        body: SingleChildScrollView(
-            child: Padding(
+        body: Padding(
           padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
           child: Column(children: [
-            Center(
-                child: Container(
-                    height: MediaQuery.of(context).size.height * .2,
-                    width: MediaQuery.of(context).size.width * .1,
-                    child: _image != null
-                        ? ClipRect(
-                            child: Image.file(
-                            _image!.absolute,
-                            height: 100,
-                            width: 100,
-                            fit: BoxFit.fitHeight,
-                          ))
-                        : Container(
-                            width: 100,
-                            height: 100,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                color: Colors.grey.shade100),
-                            child: const Icon(Icons.camera_alt,
-                                color: Colors.blue)))),
+            InkWell(
+              onTap: () {
+                dialog(context);
+              },
+              child: Center(
+                  child: SizedBox(
+                      height: MediaQuery.of(context).size.height * .2,
+                      width: MediaQuery.of(context).size.width * 1,
+                      child: _image != null
+                          ? ClipRect(
+                              child: Image.file(
+                              _image!.absolute,
+                              height: 100,
+                              width: 100,
+                              fit: BoxFit.fill,
+                            ))
+                          : Container(
+                              width: 100,
+                              height: 100,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: Colors.grey.shade100),
+                              child: const Icon(Icons.camera_alt,
+                                  color: Colors.blue)))),
+            ),
             const SizedBox(height: 30),
-            Form(
-                key: _formkey,
-                child: Column(children: [
-                  TextFormField(
-                    onChanged: (val) {
-                      title = val;
-                    },
-                    controller: titleController,
-                    keyboardType: TextInputType.text,
-                    decoration: const InputDecoration(
+            Expanded(
+              child: Form(
+                  key: _formkey,
+                  child: Column(children: [
+                    TextFormField(
+                      onChanged: (val) {
+                        title = val;
+                      },
+                      controller: titleController,
+                      keyboardType: TextInputType.text,
+                      decoration: const InputDecoration(
                         labelText: 'Blog Title',
                         border: OutlineInputBorder(),
-                        prefixIcon: Icon(Icons.email)),
-                    validator: (val) {
-                      return val!.isEmpty ? 'Enter Title' : null;
-                    },
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  TextFormField(
-                    onChanged: (val) {
-                      description = val;
-                    },
-                    validator: (val) {
-                      return val!.isEmpty ? 'Enter Description' : null;
-                    },
-                    minLines: 1,
-                    maxLength: 5,
-                    keyboardType: TextInputType.text,
-                    controller: descriptionController,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Description',
+                      ),
+                      validator: (val) {
+                        return val!.isEmpty ? 'Enter Title' : null;
+                      },
                     ),
-                  ),
-                  const Spacer(),
-                  RoundButton(title: 'UPLOAD', onPress: () {}),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                ])),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    TextFormField(
+                      onChanged: (val) {
+                        description = val;
+                      },
+                      validator: (val) {
+                        return val!.isEmpty ? 'Enter Description' : null;
+                      },
+                      minLines: 1,
+                      maxLines: 5,
+                      keyboardType: TextInputType.text,
+                      controller: descriptionController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Description',
+                      ),
+                    ),
+                    const Spacer(),
+                    RoundButton(title: 'UPLOAD', onPress: () {}),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                  ])),
+            ),
           ]),
-        )));
+        ));
   }
 }
